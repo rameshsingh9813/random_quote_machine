72e67b9056b2b5c8bc382edaec75277614000af0
Harry <rameshsingh9813@gmail.com>
2022-05-06 17:10:22 +0545

    this is the first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from 'react'
+import './App.scss';
 function App() {
+ 
+  const quotes= [
+    {
+    quote: "Life isn’t about getting and having, it’s about giving and being.",
+    author: "Kevin Kruse"
+    },
+    {
+    quote: "Whatever the mind of man can conceive and believe, it can achieve.",
+    author: "Napoleon Hill"
+    }]
+    
+    const [Quote, setQuote] = useState(quotes[0].quote);
+    const [Author, setAuthor] = useState(quotes[0].author );
+    const genRanQuote=()=>{
+      let k=Math.floor(Math.random()*quotes.length)
+      setQuote(quotes[k].quote)
+      setAuthor(quotes[k].author)
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>{Quote}</p>
+        <p>- {Author}</p>
+        <button onClick={()=> genRanQuote()}>New quote</button>
+      </header>     
     </div>
   );
 }
